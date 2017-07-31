50a56b06d3a79979988d4994057fc4aab757c265
Nifar Jafar <nigar.c@code.edu.az>
2017-07-31 15:46:31 +0400

    Doctors

---
+++ Doctors/Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Doctors.Models
+{
+    public class Category
+    {
+        [Display (Name="Kateqoriya id")]
+        public int Id { get; set; }
+
+        [Display(Name = "Kateqoriya adi")]
+        public string Name { get; set; }
+    }
+}
---
+++ Doctors/Models/CreateViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Doctors.Models
+{
+    public class CreateViewModel
+    {
+        public Doctor Doctor { get; set; }
+        public Category Category { get; set; }
+    }
+}
